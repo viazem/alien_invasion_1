84f8f2d906922b6b7fcfc69fdd6947ab579b8f8b
viazem@gmail.com <viazem@gmail.com>
2020-07-07 10:24:21 +0300

    Назначение цвета фона

--- alien_invasion.py
+++ alien_invasion.py
@@ -7,7 +7,10 @@ def run_game():
     # Инициализирует игру и создает объект экрана.
     pygame.init()
     screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion 01")
+    pygame.display.set_caption("Alien Invasion")
+
+    # Назначение цвета фона
+    bg_color = (230, 230, 230)
 
     # Запуск основного цикла игры
     while True:
@@ -15,6 +18,9 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        # При каждом проходе цикла перерисовывается экран.
+        screen.fill(bg_color)
 
         # Отображение последнего прорисованного экрана
         pygame.display.flip()
