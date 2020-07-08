224811474b56b7e88922f3389e3391cd8c35cbdf
viazem@gmail.com <viazem@gmail.com>
2020-07-08 11:14:27 +0300

    Регулировка скорости корабля. Ограничение перемещений.

--- alien_invasion.py
+++ alien_invasion.py
@@ -17,7 +17,7 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # Создаем корабль
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
 
     # Запуск основного цикла игры
     while True:
--- ship.py
+++ ship.py
@@ -3,9 +3,10 @@ import pygame
 
 class Ship():
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Инициализирует корабль и задаёт его начальную позицию"""
         self.screen = screen
+        self.ai_settings = ai_settings
 
         # Загрузка изображения корабля и получение прямоугольника
         self.image = pygame.image.load('images/ship.bmp')
@@ -14,6 +15,8 @@ class Ship():
         # Каждый новый корабль появляется у нижнего ряда экрана
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
+        # Сохранение вещественной координаты центра корабля
+        self.center = float(self.rect.centerx)
 
         # Флаг перемещения
         self.moving_right = False
@@ -21,10 +24,13 @@ class Ship():
 
     def update(self):
         """Обновляет позицию корабля с учётом флага"""
-        if self.moving_right:
-            self.rect.centerx += 1
-        if self.moving_left:
-            self.rect.centerx -= 1
+        # Обновляется атрибут center, не rect
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.center += self.ai_settings.ship_speed_factor
+        if self.moving_left and self.rect.left > 0:
+            self.center -= self.ai_settings.ship_speed_factor
+        # Обновление атрибута rect на основании self.center
+        self.rect.centerx = self.center
 
     def blitme(self):
         """Рисует корабль в текущей позиции"""
